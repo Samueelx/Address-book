b4948537e7e3b49d0ed0d716e366fd1c26011f5c
Samueelx <gsammygitau@gmail.com>
2021-08-13 00:25:58 +0300

    code refactoring

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@ function Contact(first, last){
     this.firstName = first;
     this.lastName = last;
 }
+Contact.prototype.fullName = function(){
+    return this.firstName + " " + this.lastName;
+}
 
 $(document).ready( function() {
     $('form#new-contact').submit( function(event) {
@@ -11,8 +14,16 @@ $(document).ready( function() {
         let inputLastName = $('input#new-last-name').val();
 
         let newContact = new Contact(inputFirstName, inputLastName);
+        //let fullname = newContact.fullName();
 
-        $('ul#contacts').append('<li><span class = "contact">' +newContact.firstName + '</span></li>')
+        $('ul#contacts').append('<li><span class = "contact">' +newContact.fullName() + '</span></li>');
+
+        $('.contact').last().click( function() {
+            $('#show-contact').show();
+            $('#show-contact h2').text(newContact.fullName());
+            $('#show-contact .first-name').text(newContact.firstName);
+            $('#show-contact .last-name').text(newContact.lastName);
+        });
 
         $('input#new-first-name').val('');
         $('input#new-last-name').val('');
