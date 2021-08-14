244c2703bf80ca1d05a2530a594ee3dbf80c5f7c
Samueelx <gsammygitau@gmail.com>
2021-08-14 16:37:31 +0300

    add user address

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,27 @@ function Address(street, city, county){
 }
 
 $(document).ready( function() {
+    $('#add-address').click( function() {
+        $('#new-addresses').append('<div class="new-address">'+
+            '<div class="form-group">'+
+                '<label for = "second-street">Street'+
+                    '<input type="text" class="form-control new-street" id="second-street">'
+                +'</label>'
+            +'</div>'+
+            '<div class="form-group">'+
+                '<label for = "second-city">City'+
+                    '<input type="text" class="form-control new-city" id="second-city">'
+                +'</label>'
+            +'</div>'+
+            '<div class="form-group">'+
+                '<label for = "second-county">County'+
+                    '<input type="text" class="form-control new-county" id="second-county">'
+                +'</label>'
+            +'</div>'
+        +'</div>');
+    });
+
+
     $('form#new-contact').submit( function(event) {
         event.preventDefault();
 
@@ -21,7 +42,15 @@ $(document).ready( function() {
         let inputLastName = $('input#new-last-name').val();
 
         let newContact = new Contact(inputFirstName, inputLastName);
-        //let fullname = newContact.fullName();
+        
+        $('.new-address').each( function() {
+            let inputStreet = $(this).find('input.new-street').val();
+            let inputCity = $(this).find('input.new-city').val();
+            let inputCounty = $(this).find('input.new-county').val();
+
+            let address = new Address(inputStreet, inputCity, inputCounty);
+            newContact.addresses.push(address);
+        });
 
         $('ul#contacts').append('<li><span class = "contact">' +newContact.fullName() + '</span></li>');
 
@@ -30,6 +59,11 @@ $(document).ready( function() {
             $('#show-contact h2').text(newContact.fullName());
             $('#show-contact .first-name').text(newContact.firstName);
             $('#show-contact .last-name').text(newContact.lastName);
+
+            $('ul#addresses').text("");
+            newContact.addresses.forEach( (address) => {
+                $('ul#addresses').append(`<li>${address.street}, ${address.city} ${address.county}</li>`);
+            });
         });
 
         $('input#new-first-name').val('');
